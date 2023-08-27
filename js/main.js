7e68d832a0be581faad4155884666f35aa8e5a25
Antônio Ricart <ricarteusuo11@gmail.com>
2023-08-27 17:12:11 -0300

    adiciona persistência local

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+const tbody = document.getElementsByTagName("tbody")[0];
+let alunos = getObjectLocalStorage("alunos")
+
+if (alunos === null) {
+    alunos = [];
+    setObjectLocalStorage("alunos", alunos)
+}
+
+window.addEventListener("load", () => {
+    for (aluno of alunos){
+        tbody.appendChild(createTrWithGivenValues(aluno.nome, aluno.faltas))
+    }
+})
+
 const handleSubmit = () => {
     const nome = document.getElementsByName("nome")[0];
     const faltas = document.getElementsByName("faltas")[0];
-
-    const tbody = document.getElementsByTagName("tbody")[0];
-    tbody.appendChild(createTrWithGivenValues(nome.value, faltas.value));
+    const aluno = {
+        nome: nome.value,
+        faltas: faltas.value        
+    }
     nome.value = "";
     faltas.value = "0";
+
+    alunos.push(aluno);
+    setObjectLocalStorage("alunos", alunos);
+
+    tbody.appendChild(createTrWithGivenValues(aluno.nome, aluno.faltas));
 }
+
 
 const createTrWithGivenValues = (nome, faltas) => {
     const tr = document.createElement("tr");
@@ -19,4 +40,14 @@ const createTrWithGivenValues = (nome, faltas) => {
     faltasTd.innerText = faltas;
 
     return tr;
+}
+
+
+function setObjectLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getObjectLocalStorage(key){
+    var value = localStorage.getItem(key);
+    return value && JSON.parse(value);
 }
